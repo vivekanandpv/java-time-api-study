f08a26e9f3616fa37a320c83c5488e5caa920d57
Vivekanand P V <vivekanandpv@outlook.com>
2022-04-05 21:07:14 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -1,8 +1,27 @@
 package in.athenaeum;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.TemporalAdjusters;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    //  Instant is a point on the eternal timeline
+        //  This has no meaning per se to humans
+        //  This way of profiling for performance may not be a good approach
+        Instant startInstant = Instant.now();
+
+        for (int i = 0; i < 1_000_000_000; i++) {
+            //  some code
+        }
+
+        Instant endInstant = Instant.now();
+
+        //  Duration is a measure between two Temporal instances
+        //  Instant implements Temporal
+        //  https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/time/temporal/Temporal.html
+        Duration duration = Duration.between(startInstant, endInstant);
+        System.out.println(duration.toNanos()); //  .toSeconds() was introduced in Java 9, use .getSeconds() in Java 8
     }
 }
