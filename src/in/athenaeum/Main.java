365679a44505445d7b1b584b8ccb6fcaaafd99ec
Vivekanand P V <vivekanandpv@outlook.com>
2022-04-05 21:25:42 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -3,19 +3,25 @@ package in.athenaeum;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
 
     public static void main(String[] args) {
-        //  month can also be an enum
-        //  January is value 1 (contrasting java.util.Date's zero-indexed months)
         LocalDate newYear = LocalDate.of(2022, Month.JANUARY, 1);
 
-        //  In contrast to java.util.Date, the DayOfWeek starts with Monday (value 1)
-        //  Saturday is 6 and Sunday is 7
-        DayOfWeek day = newYear.getDayOfWeek();
 
-        //  getting value
-        int dayValue = day.getValue();
+        //  All the date time apis in java.time are immutable
+        //  General additions (also check minus)
+        //  plus and minus do not mutate, but produce new instances
+        LocalDate result1 = newYear.plus(Period.ofDays(12));
+        LocalDate result2 = newYear.plus(12, ChronoUnit.DAYS);
+        LocalDate result3 = newYear.plus(Period.ofWeeks(2));
+
+        //  Specific additions (also check minus)
+        LocalDate result4 = newYear.plusDays(2);
+        LocalDate result5 = newYear.plusMonths(1);
+        LocalDate result6 = newYear.plusYears(1);
     }
 }
