c4958e8ff64dde50c4ce747c95b7816a8a09417f
Vivekanand P V <vivek@anviksha.io>
2024-07-31 14:59:52 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -8,13 +8,19 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
-        LocalDate ld = LocalDate.of(2024, 1, 1);
-        LocalDate ld1 = LocalDate.of(2024, 3, 31);
-        LocalDate ld2 = LocalDate.of(2024, 1, 1);
+        //  OffsetTime and OffsetDateTime have time offset from UTC, but this is not necessarily zone
 
-        //  these APIs work with LocalTime, LocalDateTime, ZonedDateTime as well
-        boolean r1 = ld.isBefore(ld1);
-        boolean r2 = ld1.isAfter(ld);
-        boolean r3 = ld1.isEqual(ld2);
+        //  OffsetDateTime, ZonedDateTime and Instant all store an instant on the time-line to
+        //  nanosecond precision. Instant is the simplest, simply representing the instant.
+        //  OffsetDateTime adds to the instant the offset from UTC/Greenwich, which allows
+        //  the local date-time to be obtained. ZonedDateTime adds full time-zone rules.
+
+        OffsetTime ot = OffsetTime.of(8, 30, 0, 0, ZoneOffset.ofHoursMinutes(5,30));
+
+        OffsetDateTime odt = OffsetDateTime.now();  //  only +5.30 is added in India
+
+        //  Conversion to ZonedDateTime
+        ZonedDateTime zdt1 = odt.toZonedDateTime(); //  No zone yet!, but can be added .withZoneSameInstant(ZoneId)
+        ZonedDateTime zdt2 = odt.atZoneSameInstant(ZoneId.of("Asia/Kolkata"));
     }
 }
